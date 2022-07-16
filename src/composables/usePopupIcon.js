36885a3506ee802977395b09f1ab414606c9eefa
Florent Catiau-Tristant <florent.ct@orange.fr>
2022-07-16 15:35:23 +0200

    chore: update screenshots and rename logo files

--- src/composables/usePopupIcon.js
+++ src/composables/usePopupIcon.js
@@ -4,32 +4,32 @@ export function updatePopupIcon (emojiRef) {
   watch(emojiRef, (newEmoji) => {
     updateActionIcon(newEmoji)
   })
+}
 
-  function updateActionIcon (emoji) {
-    const canvas = document.createElement('canvas')
+function updateActionIcon (emoji) {
+  const canvas = document.createElement('canvas')
 
-    const ctx = canvas.getContext('2d')
-    const baseImage = new Image()
+  const ctx = canvas.getContext('2d')
+  const baseImage = new Image()
 
-    const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    if (isDarkTheme) {
-      baseImage.src = require('../../assets/bookmark-switcher-logo.png')
+  const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+  if (isDarkTheme) {
+    baseImage.src = require('../../assets/bookmark-switcher-logo-stroke.png')
+  } else {
+    baseImage.src = require('../../assets/bookmark-switcher-logo-plain.png')
+  }
+
+  baseImage.onload = function () {
+    if (emoji) {
+      ctx.drawImage(baseImage, -10, 0)
+      ctx.font = '52px sans-serif'
+      ctx.fillText(emoji, 12, 60)
     } else {
-      baseImage.src = require('../../assets/BookmarkSwitcher-logo.png')
+      ctx.drawImage(baseImage, 0, 0)
     }
 
-    baseImage.onload = function () {
-      if (emoji) {
-        ctx.drawImage(baseImage, -10, 0)
-        ctx.font = '52px sans-serif'
-        ctx.fillText(emoji, 12, 60)
-      } else {
-        ctx.drawImage(baseImage, 0, 0)
-      }
+    const imageData = ctx.getImageData(0, 0, 64, 64)
 
-      const imageData = ctx.getImageData(0, 0, 64, 64)
-
-      browser.browserAction.setIcon({ imageData })
-    }
+    browser.browserAction.setIcon({ imageData })
   }
 }
