c51675619dd95365b605c6e1f159d07d63b947ea
Florent Catiau-Tristant <florent.ct@orange.fr>
2024-03-31 18:10:17 +0200

    fix: allow bookmarks api

--- src/manifest.ts
+++ src/manifest.ts
@@ -35,9 +35,8 @@ export async function getManifest() {
       128: './assets/icon-512.png',
     },
     permissions: [
-      'tabs',
+      'bookmarks',
       'storage',
-      'activeTab',
     ],
     host_permissions: ['*://*/*'],
     content_scripts: [
